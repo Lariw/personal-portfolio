18cb3d0657cca786fb3f815a7a8bf6d339a7834d
Kamil Hajduła <lariw@MacBook-Air-Kamil.local>
2024-03-07 02:22:45 +0100

    Customize for responsible view.

---
+++ script/more-info.js
@@ -0,0 +1,10 @@
+(() => {
+
+    const moreInfoBtn = document.querySelector('.js-menuRoll');
+    const moreInfoContainer = document.querySelector('.aside__moreInfo');
+    moreInfoBtn.addEventListener('click', () => {
+        moreInfoContainer.classList.toggle('aside__moreInfo--active')
+    })
+
+
+})()
