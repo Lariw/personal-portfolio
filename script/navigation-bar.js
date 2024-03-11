d3284a6b31bff0efbf276ca7c19948012fe08951
Kamil Hajduła <lariw@MacBook-Air-Kamil.local>
2024-03-11 01:45:45 +0100

    Added sections, contact form.

--- script/navigation-bar.js
+++ script/navigation-bar.js
@@ -1,5 +1,7 @@
 (() => {
     const navigationBtns = document.querySelectorAll('.nav__hreflink');
+    const articles = document.querySelectorAll('article');
+
     navigationBtns.forEach((btn) => {
         btn.addEventListener('click', () => {
 
@@ -7,6 +9,15 @@
                 btn.classList = "nav__hreflink"
             })
 
+            const sectionName = btn.innerHTML.toLowerCase();
+
+            console.log(sectionName)
+
+            articles.forEach((article) => {
+                article.classList.remove('active');
+            })
+
+            document.querySelector('.' + sectionName).classList.add('active')
             btn.classList.toggle('nav__hreflink--active')
         })
     })
