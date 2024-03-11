a1398c885c1c3c90b6ddcc14488fb5c724229843
Kamil Hajduła <98982966+Lariw@users.noreply.github.com>
2024-03-11 11:20:24 +0100

    Added LinkedIn icon, some tidy up code.

--- script/more-info.js
+++ script/more-info.js
@@ -1,10 +1,7 @@
 (() => {
-
-    const moreInfoBtn = document.querySelector('.js-menuRoll');
-    const moreInfoContainer = document.querySelector('.aside__moreInfo');
-    moreInfoBtn.addEventListener('click', () => {
-        moreInfoContainer.classList.toggle('aside__moreInfo--active')
-    })
-
-
-})()
+  const moreInfoBtn = document.querySelector(".js-menuRoll");
+  const moreInfoContainer = document.querySelector(".aside__moreInfo");
+  moreInfoBtn.addEventListener("click", () => {
+    moreInfoContainer.classList.toggle("aside__moreInfo--active");
+  });
+})();
--- script/navigation-bar.js
+++ script/navigation-bar.js
@@ -1,24 +1,23 @@
 (() => {
-    const navigationBtns = document.querySelectorAll('.nav__hreflink');
-    const articles = document.querySelectorAll('article');
+  const navigationBtns = document.querySelectorAll(".nav__hreflink");
+  const articles = document.querySelectorAll("article");
 
-    navigationBtns.forEach((btn) => {
-        btn.addEventListener('click', () => {
+  navigationBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      navigationBtns.forEach((btn) => {
+        btn.classList = "nav__hreflink";
+      });
 
-            navigationBtns.forEach((btn) => {
-                btn.classList = "nav__hreflink"
-            })
+      const sectionName = btn.innerHTML.toLowerCase();
 
-            const sectionName = btn.innerHTML.toLowerCase();
+      console.log(sectionName);
 
-            console.log(sectionName)
+      articles.forEach((article) => {
+        article.classList.remove("active");
+      });
 
-            articles.forEach((article) => {
-                article.classList.remove('active');
-            })
-
-            document.querySelector('.' + sectionName).classList.add('active')
-            btn.classList.toggle('nav__hreflink--active')
-        })
-    })
-})()
+      document.querySelector("." + sectionName).classList.add("active");
+      btn.classList.toggle("nav__hreflink--active");
+    });
+  });
+})();
