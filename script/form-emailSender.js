9f9f7dde801b1819fa51c2301812170e3fb23bfa
Kamil Hajduła <98982966+Lariw@users.noreply.github.com>
2024-04-23 11:08:36 +0200

    Added some elements in about me section

--- script/form-emailSender.js
+++ script/form-emailSender.js
@@ -1,1 +1,18 @@
-const sendMail=(e,t,r)=>{let l={fullName:e,email:t,message:r};emailjs.send("service_d74r2md","template_w3bn7aa",l)},form=document.querySelector("form");form.addEventListener("submit",e=>{e.preventDefault();let t=document.querySelector(".js-fullName").value,r=document.querySelector(".js-email").value,l=document.querySelector(".js-message").value;sendMail(t,r,l);let n=document.querySelector(".js-submitBtnContent");form.reset(),n.innerText="Thank you!",setTimeout(()=>{n.innerText="Send Message"},5e3)});
+const sendMail = (e, t, r) => {
+    let l = { fullName: e, email: t, message: r };
+    emailjs.send("service_d74r2md", "template_w3bn7aa", l);
+  },
+  form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let t = document.querySelector(".js-fullName").value,
+    r = document.querySelector(".js-email").value,
+    l = document.querySelector(".js-message").value;
+  sendMail(t, r, l);
+  let n = document.querySelector(".js-submitBtnContent");
+  form.reset(),
+    (n.innerText = "Thank you!"),
+    setTimeout(() => {
+      n.innerText = "Send Message";
+    }, 5e3);
+});
